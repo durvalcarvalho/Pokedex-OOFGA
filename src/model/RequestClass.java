2fcc7169d8cb4d0e4a18a19703951815c217988f
Durval Carvalho <durvalcsouza@outlook.com>
2018-11-04 20:37:29 -0200

    Mudanças na classe Request

--- src/model/RequestClass.java
+++ src/model/RequestClass.java
@@ -1,69 +1,44 @@
 package model;
 
 import java.io.IOException;
+
+import org.apache.http.client.HttpResponseException;
 import org.apache.http.client.fluent.Request;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-
 /**
  * A classe RequestClass implementa os métodos de requisição https.
  *
- * Toda classe filha da RequestClass pode usar os métodos get e parse
+ * Toda classe que herdar da RequestClass pode usar os métodos get e parse
  * para coletar dados da API pokemon.
  */
 public abstract class RequestClass
 {
-	protected String nome; 	// nomed pokemon
-	protected int ID;		// id do pokemon
+	protected String nome;
+	protected int ID;
 	
-	// TODO: Conferir se está sendo usado e remover
 	public String getNome() { return nome; 	}
 	public int getID() 		{ return ID; 	}
 	
 	
 	/* Pegar String com todos os dados do pokemon e retornar em um JSONObject */
-	protected JSONObject parse(String data)
+	protected JSONObject parse(String data) throws JSONException
 	{
 		JSONObject root;
+		root = new JSONObject(data);
 		
-		try
-		{
-			root = new JSONObject(data);
-			return root;
-		}
-		catch (JSONException e)
-		{
-			// TODO: Tratar este erro!
-			e.printStackTrace();
-		}
-		
-		return null;
+		return root;
 	}
 	
 	
 	/* Fazer a requisição Https e retornar os dados coletados em uma string */
-	protected String get(String url)
+	protected String get(String url)  throws HttpResponseException, IOException, IllegalStateException
 	{
 		String data = "";
 		
-		try
-		{
-			data = Request.Get(url).execute().returnContent().asString();
-		}
-		
-		catch (IOException e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} 
-		
-		catch(IllegalStateException e)
-		{
-			//TODO: Tratar error: URL INVÁLIDA
-			e.printStackTrace(); 
-		}
-		
+		data = Request.Get(url).execute().returnContent().asString();
+	
 		return data;
 	}
 }

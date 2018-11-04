5787f1cba8b3d0c50dd3c485009cecf6001ef3bc
Durval Carvalho <durvalcsouza@outlook.com>
2018-11-04 20:40:51 -0200

    Adição da Classe para testar Pokemon Tipos

---
+++ src/test/Testa_Pokemon_Tipo.java
@@ -0,0 +1,35 @@
+package testes;
+
+import java.util.Vector;
+
+import model.Pokemon;
+import model.Pokemon_Tipo;
+
+public class Testa_Pokemon_Tipo
+{
+	public static void main(String[] args)
+	{
+		long startTime = System.nanoTime();
+
+		Pokemon_Tipo pt = new Pokemon_Tipo();
+		
+		Vector<String> a = pt.Pokemon_type("fire");
+		
+		for(int i=0; i<a.size(); i++)
+		{
+			System.out.println(a.get(i));
+		}
+		
+		long endTime   = System.nanoTime();
+		long totalTime = endTime - startTime;
+		
+		System.out.println(totalTime/1000000000 + " Segundos");
+		
+	}
+	
+	// 24 segundos
+	// 24 segundos
+	
+	
+	
+}

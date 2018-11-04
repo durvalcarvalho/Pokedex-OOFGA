06ac33e51f98ab739ecb1c8d31b53cce359bcd9c
Durval Carvalho <durvalcsouza@outlook.com>
2018-11-04 20:45:33 -0200

    Mudanças na tela Buscador Pokemon

--- src/view/Buscador_Pokemon.java
+++ src/view/Buscador_Pokemon.java
@@ -1,137 +1,290 @@
 package view;
 
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
+
+import org.apache.http.client.HttpResponseException;
+import org.json.JSONException;
+
+import model.Pokemon;
+import model.Pokemon_Tipo;
+
 import javax.swing.JLabel;
 import java.awt.Font;
 import java.awt.Choice;
 import javax.swing.JTextField;
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import java.awt.FlowLayout;
+import javax.swing.JDialog;
+
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Vector;
 import java.awt.event.ActionEvent;
+import java.awt.Color;
 
-public class Buscador_Pokemon extends JFrame
+@SuppressWarnings("serial")
+public class Buscador_Pokemon extends JDialog
 {
-
+	// Elementos da tela
 	private JPanel Buscador_Panel;
 	private JTextField Pokemon_Nome_textField;
-
-	public static void main(String[] args)
+	private JLabel Pokemon_Img;
+	private JLabel lblPokemonNoEncontrado;
+	private Choice Pokemon_achados_choice;
+	
+	private Vector<String> poke_names = new Vector<String>();
+	
+	private Pokemon atual_pokemon = null;
+	
+	public String getPokemon()
 	{
-		EventQueue.invokeLater(new Runnable()
-		{
-			public void run()
-			{
-				try
-				{
-					Buscador_Pokemon frame = new Buscador_Pokemon();
-					frame.setVisible(true);
-				}
-				catch (Exception e)
-				{
-					e.printStackTrace();
-				}
-			}
-		});
+		if(atual_pokemon != null)
+			return atual_pokemon.getNome();
+		else
+			return null;
 	}
 	
+	// TODO: REMOVER DEPOIS
+	public static void main(String[] args)
+	{
+		try
+		{
+			Buscador_Pokemon frame = new Buscador_Pokemon();
+		}
+		
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
 	public Buscador_Pokemon()
 	{
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// O escopo onde foi chamado esse Buscador_Pokemon ficará
+		// aguardando enquanto esse JDialog estiver aberto
+		setModal(true);
+
+		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 		setBounds(350, 150, 650, 500);
 		Buscador_Panel = new JPanel();
 		Buscador_Panel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(Buscador_Panel);
 		Buscador_Panel.setLayout(null);
-		Buscador_Panel.setLayout(null);
-		
+
 		JLabel ProcurarPokemon_lbl = new JLabel("Procurar Pokemon");
-		ProcurarPokemon_lbl.setFont(new Font("Ubuntu Mono", Font.BOLD, 20));
+		ProcurarPokemon_lbl.setFont(new Font("Ubuntu Mono",
+				Font.BOLD, 20));
 		ProcurarPokemon_lbl.setBounds(230, 28, 168, 20);
 		Buscador_Panel.add(ProcurarPokemon_lbl);
-		
+
 		Choice Pokemon_choice_Tipo = new Choice();
 		Pokemon_choice_Tipo.setBounds(100, 110, 430, 20);
 		Buscador_Panel.add(Pokemon_choice_Tipo);
-		
+
+		String[] tipos_conhecidos = {"normal","fighting","flying",
+				"poison","ground","rock","bug","ghost","steel",
+				"fire","water","grass","electric","psychic","ice",
+				"dragon","dark","fairy","shadow"};
+
+		for(int i=0; i<tipos_conhecidos.length; i++)
+		{
+			Pokemon_choice_Tipo.add(tipos_conhecidos[i].toUpperCase());
+		}
+
 		JLabel Tipo_lbl = new JLabel("Tipo");
 		Tipo_lbl.setBounds(300, 80, 30, 15);
 		Buscador_Panel.add(Tipo_lbl);
-		
+
 		JLabel Nome_lbl = new JLabel("Nome");
 		Nome_lbl.setBounds(300, 150, 40, 15);
 		Buscador_Panel.add(Nome_lbl);
-		
+
 		Pokemon_Nome_textField = new JTextField();
 		Pokemon_Nome_textField.setBounds(100, 180, 430, 19);
 		Buscador_Panel.add(Pokemon_Nome_textField);
 		Pokemon_Nome_textField.setColumns(10);
 		
+		// Esse é texto é acionado quando o pokemon não é encontrado
+		lblPokemonNoEncontrado = new JLabel("Pokemon Não Encontrado! :(");
+		lblPokemonNoEncontrado.setForeground(Color.BLUE);
+		lblPokemonNoEncontrado.setBounds(350, 270, 205, 15);
+		Buscador_Panel.add(lblPokemonNoEncontrado);
+		lblPokemonNoEncontrado.setVisible(false); // default é desligado
+		
+		Pokemon_Img = new JLabel("Pokemon_Name");
+		Pokemon_Img.setIcon(new ImageIcon(Pokedex.class.getResource("/view/Imagens/0.png")));
+		Pokemon_Img.setBounds(400, 290, 100, 100);
+		Buscador_Panel.add(Pokemon_Img);
+		
+		Pokemon_achados_choice = new Choice();
+		Pokemon_achados_choice.setBounds(100, 340, 200, 25);
+		Buscador_Panel.add(Pokemon_achados_choice);
+		
+		/* BOTÕES */
+
+		// Este botão irá buscar um pokemon ou um conjunto de pokemons de um determinado tipo
 		JButton BuscarPokemon_btn = new JButton("Buscar Pokemon");
 		BuscarPokemon_btn.addActionListener(new ActionListener()
 		{
 			public void actionPerformed(ActionEvent e)
 			{
-				// TODO: Fazer a busca de acordo com os parâmetros passados
-				// TODO: Tratar os erros ( nome inválido, nome e tipo vazios)
+				// O texto de erro é 'desligada' caso tenha sido
+				// acionada em buscas anteriores
+				lblPokemonNoEncontrado.setVisible(false);
+				
+				// Trocar a imagem de erro para a imagem padrão 
+				Pokemon_Img.setIcon(new ImageIcon(Pokedex.class
+						.getResource("/view/Imagens/0.png")));
+				
+				// Texto digitado no campo de texto
+				String poke_textField = Pokemon_Nome_textField
+						.getText();
+
+				// Se o campo de texto estiver vazio é feito uma pesquisa por tipo
+				if(poke_textField.isEmpty())
+				{
+					String tipo = Pokemon_choice_Tipo.getSelectedItem();
+					
+					// Remover do "log" pesquisas anteriores
+					poke_names.clear();
+					
+					// Classe que realiza a busca por tipo
+					Pokemon_Tipo pt = new Pokemon_Tipo();
+					
+					// Método que realiza as requisições dos pokemons de um determinado tipo
+					poke_names = pt.Pokemon_type(tipo.toLowerCase());
+					
+					// Método para mostrar os pokemons filtrados por tipo
+					choice_pokemons();
+				}
+
+				// Caso tenha alguma coisa digitada, buscar pelo nome do pokemon
+				else
+				{
+					try
+					{
+						// Tentar criar um pokemon com o texto digitado
+						Pokemon pokemon = new Pokemon(poke_textField);
+						
+						// Remover do "log" as pesquisas anteriores
+						poke_names.clear();
+						
+						// Adicionar o nome do pokemon pesquisado no log
+						poke_names.add(pokemon.getNome());
+						
+						// Mudar a imagem do pokemon
+						changeImage(pokemon.getNome());			
+					}
+					
+					// Caso o nome digitado não seja válido entra nesta exceção
+					catch (HttpResponseException e1)
+					{
+						// Texto de aviso que pokemon não foi encontrado
+						lblPokemonNoEncontrado.setVisible(true);
+						
+						// Imagem de apoio - Pokemon não encontrado
+						Pokemon_Img.setIcon(new ImageIcon
+								(Pokedex.class.getResource
+										("/view/Imagens/not_found.png")));
+					}
+					
+					// Outras exceção relacionada com a requisição
+					catch (IllegalStateException | JSONException
+							| IOException e1)
+					{
+						e1.printStackTrace();
+					}
+				}
 			}
 		});
 		BuscarPokemon_btn.setBounds(240, 230, 160, 25);
-		Buscador_Panel.add(BuscarPokemon_btn);
-				
-		// TODO: Implementar o método para pegar a imagem de acordo com o pokemon selecioado
-		// TODO: Essa tela deve iniciar sem nenhuma imagem
-		JLabel Pokemon_Img = new JLabel("Pokemon_Name");
-		Pokemon_Img.setIcon(new ImageIcon(Pokedex.class.getResource("/view/Imagens/5.png")));
-		Pokemon_Img.setBounds(400, 290, 100, 100);
-		Buscador_Panel.add(Pokemon_Img);
-		
+		Buscador_Panel.add(BuscarPokemon_btn);		
+
+		// Este botão verifica se algum pokemon foi selecionado
+		// Se tiver sido, retorna para pokedex, caso contrário não faz nada
 		JButton Capturar_Pokemon_button = new JButton("Capturar Pokemon");
 		Capturar_Pokemon_button.addActionListener(new ActionListener()
 		{
 			public void actionPerformed(ActionEvent e)
 			{
-				// TODO: Adicionar o pokemon encontrado a lista de pokemons capturados do treinador
+				// Se algum pokemon estiver selecionado, retorno para pokedex
+				if(!(atual_pokemon == null))
+				{
+					dispose();
+				}
 			}
 		});
-		Capturar_Pokemon_button.setBounds(240, 440, 190, 25);
+		Capturar_Pokemon_button.setBounds(230, 440, 190, 25);
 		Buscador_Panel.add(Capturar_Pokemon_button);
 		
-		// TODO: Descobrir como fazer uma lista rolável com os botões
-		JPanel Pokemons_Filtrados_Panel = new JPanel();
-		Pokemons_Filtrados_Panel.setBounds(100, 310, 215, 101);
-		Buscador_Panel.add(Pokemons_Filtrados_Panel);
-		Pokemons_Filtrados_Panel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-				
-		
-		JButton buttons[] = new JButton[20];
-		for(int i=0; i<20; i++)
+		JButton show_pokemon_btn = new JButton("Visualizar");
+		show_pokemon_btn.addActionListener(new ActionListener()
 		{
-			buttons[i] = new JButton("Pokemon "+i);
-			Pokemons_Filtrados_Panel.add(buttons[i]);
-		}
-		
-		
-		/*
-		// TODO: Este método irá encontrar os pokemons
-		  		 e criar os botões referentes a cada um
-		  		 adicionar ao panel
-		  		 
-		encontra_pokemons(Pokemons_Filtrados_Panel);
-		{
-			for(int i=0; i<qnt_pokemons_encontrados(); i++)
+			public void actionPerformed(ActionEvent e)
 			{
+				lblPokemonNoEncontrado.setVisible(false);
 				
+				// Se alguma busca tiver sido feita, é chamado o método para mudar a imagem
+				if(!(poke_names.isEmpty()))
+				{
+					changeImage(Pokemon_achados_choice.getSelectedItem());
+				}
 			}
+		});
+		show_pokemon_btn.setBounds(140, 310, 120, 25);
+		Buscador_Panel.add(show_pokemon_btn);	
+		
+		// Método para tornar this.JDialog visível
+		setVisible(true);
+	}
+	
+	// Método para mostrar os pokemons filtrados por tipo
+	private void choice_pokemons()
+	{
+		// Toda vez que esse método é acionado é resetado a lista
+		// de pokemons de buscas anteriores
+		Pokemon_achados_choice.removeAll();
+		
+		for(int i=0; i<poke_names.size(); i++)
+		{
+			Pokemon_achados_choice.add(poke_names.elementAt(i));
+		}	
+	}
+	
+	// Método para mudar a imagem de acordo com o nome do pokemon
+	private void changeImage(String pokemon_name)
+	{
+		int id;
+
+		try
+		{
+			atual_pokemon = new Pokemon(pokemon_name);
+			
+			id = (atual_pokemon.getPoke_Id());
+
+			URL url = new URL("https://raw.githubusercontent.com"
+					+ "/PokeAPI/sprites/master/sprites/pokemon/"
+					+id+".png");
+
+			BufferedImage image = ImageIO.read(url);
+
+			Pokemon_Img.setIcon(new ImageIcon(image));
 		}
-		*/
 		
-		
+		// Se algum erro ocorrer é mostrado uma imagem de erro
+		catch (IllegalStateException | JSONException | IOException e)
+		{
+			// Texto de aviso que pokemon não foi encontrado
+			lblPokemonNoEncontrado.setVisible(true);
+			
+			// Imagem de apoio - Pokemon não encontrado
+			Pokemon_Img.setIcon(new ImageIcon
+					(Pokedex.class.getResource
+							("/view/Imagens/not_found.png")));
+		}
 	}
 }

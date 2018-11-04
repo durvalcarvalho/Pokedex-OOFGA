1e9a5f3d29f5d113ddd06a23bbeed84bc5d7acef
Durval Carvalho <durvalcsouza@outlook.com>
2018-11-04 20:44:22 -0200

    Mudanças na classe Habilidades Pokemon

--- src/view/Habilidades_Pokemon.java
+++ src/view/Habilidades_Pokemon.java
@@ -1,44 +1,30 @@
 package view;
 
 import java.awt.Color;
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import model.Pokemon;
 import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.JTextPane;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Choice;
 
+//TODO: Colocar uma imagem do pokemon em questão
+
+@SuppressWarnings("serial")
 public class Habilidades_Pokemon extends JFrame
 {
 
 	private JPanel Habilidades_Panel;
-
-	public static void main(String[] args)
+	
+	public Habilidades_Pokemon(Pokemon pokemon)
 	{
-		EventQueue.invokeLater(new Runnable()
-		{
-			public void run()
-			{
-				try
-				{
-					Habilidades_Pokemon frame = new Habilidades_Pokemon();
-					frame.setVisible(true);
-				}
-				catch (Exception e)
-				{
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	public Habilidades_Pokemon()
-	{
+		pokemon.load_abilities();
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(550, 150, 450, 500);
 		Habilidades_Panel = new JPanel();
@@ -51,8 +37,7 @@ public class Habilidades_Pokemon extends JFrame
 		Pokemon_lbl.setBounds(5, 5, 70, 15);
 		Habilidades_Panel.add(Pokemon_lbl);
 		
-		// TODO: Criar um método para mudar o nome do pokemon
-		JLabel Pokemon_Name_lbl = new JLabel("Pikachu");
+		JLabel Pokemon_Name_lbl = new JLabel(pokemon.getNome());
 		Pokemon_Name_lbl.setForeground(Color.BLUE);
 		Pokemon_Name_lbl.setFont(new Font("Ubuntu Mono", Font.BOLD, 14));
 		Pokemon_Name_lbl.setBounds(75, 5, 200, 15);
@@ -63,13 +48,8 @@ public class Habilidades_Pokemon extends JFrame
 		Habilidades_lbl.setBounds(147, 45, 160, 20);
 		Habilidades_Panel.add(Habilidades_lbl);
 		
-		// TODO: Este painel irá receber os botões que acionam a decrição da habilidade
-		JPanel Nome_Habilidade_panel = new JPanel();
-		Nome_Habilidade_panel.setBounds(75, 130, 300, 100);
-		Habilidades_Panel.add(Nome_Habilidade_panel);
-		
 		JTextPane Descricao_txtpanel = new JTextPane();
-		Descricao_txtpanel.setText("The target is hit with a destructive shock wave that\\nalways inflicts 20 HP damage.");
+		Descricao_txtpanel.setText("");
 		Descricao_txtpanel.setBounds(75, 263, 300, 100);
 		Habilidades_Panel.add(Descricao_txtpanel);
 		
@@ -78,10 +58,33 @@ public class Habilidades_Pokemon extends JFrame
 		{
 			public void actionPerformed(ActionEvent e)
 			{
-				//TODO: Fechar a tela e voltar para a pokedex
+				setVisible(false);
 			}
 		});
 		Voltar_btn.setBounds(160, 425, 120, 25);
-		Habilidades_Panel.add(Voltar_btn);
+		Habilidades_Panel.add(Voltar_btn);		
+		
+		Choice Habilidade_choice = new Choice();
+		Habilidade_choice.setBounds(75, 170, 300, 25);
+		Habilidades_Panel.add(Habilidade_choice);
+		
+		JButton Descricao_btn = new JButton("Descrição");
+		Descricao_btn.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				int index = Habilidade_choice.getSelectedIndex();
+				Descricao_txtpanel.setText(pokemon.getHabilidades().get(index).getValue());
+			}
+		});
+		Descricao_btn.setBounds(160, 210, 117, 25);
+		Habilidades_Panel.add(Descricao_btn);
+		
+		for(int i=0; i<pokemon.getHabilidades().size(); i++)
+		{			
+			Habilidade_choice.add(pokemon.getHabilidades().get(i).getKey());
+		}
+		
+		setVisible(true);
 	}
 }
